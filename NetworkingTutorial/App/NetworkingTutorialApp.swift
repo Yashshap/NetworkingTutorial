47518f9b9d6e4b251b84d08c6c7dd30e37b3f65d
yash <yash.shapane@Logituit.com>
2025-02-21 14:53:33 +0530

    asd

--- NetworkingTutorial/App/NetworkingTutorialApp.swift
+++ NetworkingTutorial/App/NetworkingTutorialApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct NetworkingTutorialApp: App {
+    @StateObject var viewModel = CoinsViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
